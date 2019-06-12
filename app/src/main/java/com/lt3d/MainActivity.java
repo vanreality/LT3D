9cdbe7af0ee04fff88955110ffa5c7894cc966bd
fan <vanshiyu@gmail.com>
2019-06-12 22:32:11 +0200

    Firebase signin with email, read data in the realtime database

--- app/src/main/java/com/lt3d/MainActivity.java
+++ app/src/main/java/com/lt3d/MainActivity.java
@@ -51,9 +51,12 @@ public class MainActivity extends AppCompatActivity {
     public void createFirebaseSignInIntent() {
         // [START auth_fui_create_intent]
         // Choose authentication providers
+//        List<AuthUI.IdpConfig> providers = Arrays.asList(
+//                new AuthUI.IdpConfig.EmailBuilder().build(),
+//                new AuthUI.IdpConfig.GoogleBuilder().build());
+
         List<AuthUI.IdpConfig> providers = Arrays.asList(
-                new AuthUI.IdpConfig.EmailBuilder().build(),
-                new AuthUI.IdpConfig.GoogleBuilder().build());
+                new AuthUI.IdpConfig.EmailBuilder().build());
 
         // Create and launch sign-in intent
         startActivityForResult(
