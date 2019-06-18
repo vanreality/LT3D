213d26651fba6a8222ef113661570936ac9be2a3
weianqi <weianqi5@gmail.com>
2019-06-18 10:13:38 +0200

    model形成了Arraylist

--- app/src/main/java/com/lt3d/tools/AugmentedImageNode.java
+++ app/src/main/java/com/lt3d/tools/AugmentedImageNode.java
@@ -29,6 +29,8 @@ import com.google.ar.sceneform.rendering.ModelRenderable;
 import com.google.ar.sceneform.ux.ArFragment;
 import com.lt3d.fragment.ScanFragment;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 /**
@@ -43,6 +45,7 @@ public class AugmentedImageNode extends AnchorNode {
   private AugmentedImage image;
   private String imgName;
   private String nodeName;
+  private List<String> nodeNames=new ArrayList<>();
   private String nameSfb;
 
   // Models of the 4 corners.  We use completable futures here to simplify
@@ -53,46 +56,93 @@ public class AugmentedImageNode extends AnchorNode {
 //  private static CompletableFuture<ModelRenderable> lrCorner;
 //  private static CompletableFuture<ModelRenderable> llCorner;
 
+  private static List<CompletableFuture<ModelRenderable>> myModels=new ArrayList<>();
   private static CompletableFuture<ModelRenderable> myModel;
 
   public AugmentedImageNode(Context context,String nodeName) {
     // Upon construction, start loading the models for the corners of the frame.
 
-    if(nodeName.equals("dog.png")){
-      nameSfb ="model/frame_lower_left.sfb";
-    }
+    nodeNames.add("dog.png");
+    myModel =
+            ModelRenderable.builder()
+                    .setSource(context, Uri.parse("dog/12228_Dog_v1_L2.sfb"))
+                    .build();
+    myModels.add(myModel);
+    nodeNames.add("skull3.jpg");
+    myModel =
+            ModelRenderable.builder()
+                    .setSource(context, Uri.parse("skull/12140_Skull_v3_L2.sfb"))
+                    .build();
+    myModels.add(myModel);
+    nodeNames.add("venus.png");
+    myModel =
+            ModelRenderable.builder()
+                    .setSource(context, Uri.parse("venus/12328_Statue_v1_L2.sfb"))
+                    .build();
+    myModels.add(myModel);
+    nodeNames.add("egypt_lion.png");
+    myModel =
+            ModelRenderable.builder()
+                    .setSource(context, Uri.parse("egypt/10085_egypt_sphinx_iterations-2.sfb"))
+                    .build();
+    myModels.add(myModel);
+    nodeNames.add("deer.png");
+    myModel =
+            ModelRenderable.builder()
+                    .setSource(context, Uri.parse("deer/12961_White-Tailed_Deer_v1_l2.sfb"))
+                    .build();
+    myModels.add(myModel);
 
-    else if (nodeName.equals("skull3.jpg")){
-      nameSfb = "skull/12140_Skull_v3_L2.sfb";
-    }
+    nodeNames.add("ironman.png");
+    myModel =
+            ModelRenderable.builder()
+                    .setSource(context, Uri.parse("ironman/IronMan.sfb"))
+                    .build();
+    myModels.add(myModel);
 
-    else if(nodeName.equals("venus.png")){
-      nameSfb = "venus/12328_Statue_v1_L2.sfb";
-    }
-
-    else if(nodeName.equals("egypt_lion.png")){
-      nameSfb = "egypt/10085_egypt_sphinx_iterations-2.sfb";
-    }
-
-    else if(nodeName.equals("deer.png")){
-      nameSfb = "deer/12961_White-Tailed_Deer_v1_l2.sfb";
-    }
-
-    else if(nodeName.equals("ironman.png")){
-      nameSfb = "ironman/IronMan.sfb";
-    }
-
-    else{
-      nameSfb="model/frame_lower_left.sfb";
-    }
-
-      if(myModel == null){
-        myModel =
-                ModelRenderable.builder()
-                        .setSource(context, Uri.parse(nameSfb))
-                        .build();
-
+    for(int i=0;i<nodeNames.size();i++){
+      if(nodeNames.get(i).equals(nodeName)){
+        myModel=myModels.get(i);
       }
+    }
+
+
+
+//    if(nodeName.equals("dog.png")){
+//      nameSfb ="model/frame_lower_left.sfb";
+//    }
+//
+//    else if (nodeName.equals("skull3.jpg")){
+//      nameSfb = "skull/12140_Skull_v3_L2.sfb";
+//    }
+//
+//    else if(nodeName.equals("venus.png")){
+//      nameSfb = "venus/12328_Statue_v1_L2.sfb";
+//    }
+//
+//    else if(nodeName.equals("egypt_lion.png")){
+//      nameSfb = "egypt/10085_egypt_sphinx_iterations-2.sfb";
+//    }
+//
+//    else if(nodeName.equals("deer.png")){
+//      nameSfb = "deer/12961_White-Tailed_Deer_v1_l2.sfb";
+//    }
+//
+//    else if(nodeName.equals("ironman.png")){
+//      nameSfb = "ironman/IronMan.sfb";
+//    }
+//
+//    else{
+//      nameSfb="model/frame_lower_left.sfb";
+//    }
+//
+//      if(myModel == null){
+//        myModel =
+//                ModelRenderable.builder()
+//                        .setSource(context, Uri.parse(nameSfb))
+//                        .build();
+//
+//      }
 
 
 
@@ -159,7 +209,7 @@ public class AugmentedImageNode extends AnchorNode {
 
     localPosition.set(0.0f, 0.0f, 0.0f);
       cornerNode = new Node();
-      cornerNode.setLocalRotation(Quaternion.axisAngle(new Vector3(1f, 0, 0), 180f));
+      cornerNode.setLocalRotation(Quaternion.axisAngle(new Vector3(1f, 0, 0), 270f));
       cornerNode.setParent(this);
       cornerNode.setLocalPosition(localPosition);
       cornerNode.setRenderable(myModel.getNow(null));
